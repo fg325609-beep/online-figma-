2ba85e7b3cc39e527fbcc8614c66b6f14295b6c7
Farhod Gofurov <fg325609@gmail.com>
2026-05-24 06:52:29 +0500

    lllll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './companents/Header/Header'
 import './i18n';
+import { Hero } from './companents/Hero/Hero';
 const App = () => {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   )
 }
--- src/companents/Hero/Hero.jsx
+++ src/companents/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./Hero.scss"
+import { useTranslation } from 'react-i18next';
 
 export const Hero = () => {
+     const { t, i18n } = useTranslation();
   return (
     <div>
-        
+        <section>
+            <div className="hero">
+                <h1>{t('hero.title')}</h1>
+                <p>{t('hero.description')}</p>
+                <button type="submit">{t('hero.button')}</button>
+            </div>
+        </section>
     </div>
   )
 }

6dffd3c86da92986cc0e6897ab1a6bd9824fac5e
Farhod Gofurov <fg325609@gmail.com>
2026-05-25 07:54:13 +0500

    llll

--- src/companents/Header/Header.jsx
+++ src/companents/Header/Header.jsx
@@ -4,17 +4,21 @@ import './Header.scss';
 
 const Header = () => {
     const { t, i18n } = useTranslation();
-    
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-    const chenglanguage = (lang) => {
+    const changeLanguage = (lang) => {
         if (i18n && typeof i18n.changeLanguage === 'function') {
             i18n.changeLanguage(lang);
         }
+    };
+
+    // Har safar link bosilganda menyu yopilishi uchun funksiya
+    const handleLinkClick = () => {
+        setIsMenuOpen(false);
     };
 
     return (
@@ -23,30 +27,33 @@ const Header = () => {
                 <div className="navbar">
                     <h1 className='logo'>{t('navbar.logo')}</h1>
                     
+                    {/* NAV-MENU endi ham ul-ro'yxatni, ham tugmalarni ichiga oladi */}
                     <nav className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
-                        <nav>
-                            <li><a href="#home">{t('navbar.links.home')}</a></li>
-                            <li><a href="#courses">{t('navbar.links.courses')}</a></li>
-                            <li><a href="#page">{t('navbar.links.page')}</a></li>
-                            <li><a href="#blog">{t('navbar.links.blog')}</a></li>
-                            <li><a href="#contact">{t('navbar.links.contact')}</a></li>
-                        </nav>
+                        <ul className="nav-list">
+                            <li><a href="#home" onClick={handleLinkClick}>{t('navbar.links.home')}</a></li>
+                            <li><a href="#courses" onClick={handleLinkClick}>{t('navbar.links.courses')}</a></li>
+                            <li><a href="#page" onClick={handleLinkClick}>{t('navbar.links.page')}</a></li>
+                            <li><a href="#blog" onClick={handleLinkClick}>{t('navbar.links.blog')}</a></li>
+                            <li><a href="#contact" onClick={handleLinkClick}>{t('navbar.links.contact')}</a></li>
+                        </ul>
+
+                        {/* Tugmalar va til tanlash endi nav-menu ichida (ul dan tashqarida) */}
+                        <div className="nav-actions">
+                            <a className='nav-a' href="#signin" onClick={handleLinkClick}>{t('navbar.actions.signIn')}</a>
+                            <a className='nav-a signup-btn' href="#signup" onClick={handleLinkClick}>{t('navbar.actions.signUp')}</a>
+                            
+                            <select 
+                                onChange={(e) => changeLanguage(e.target.value)} 
+                                defaultValue={i18n ? i18n.language : "uz"}
+                            >
+                                <option value="uz">UZ</option>
+                                <option value="en">EN</option>
+                                <option value="ru">RU</option>
+                            </select>
+                        </div>
                     </nav>
 
-                    <span className="nav-actions">
-                        <a className='nav-a' href="#signin">{t('navbar.actions.signIn')}</a>
-                        <a className='nav-a signup-btn' href="#signup">{t('navbar.actions.signUp')}</a>
-                        
-                        <select 
-                            onChange={(e) => chenglanguage(e.target.value)} 
-                            defaultValue={i18n ? i18n.language : "uz"}
-                        >
-                            <option value="uz">UZ</option>
-                            <option value="en">EN</option>
-                            <option value="ru">RU</option>
-                        </select>
-                    </span>
-
+                    {/* Gamburger tugmasi */}
                     <button 
                         className={isMenuOpen ? "hamburger active" : "hamburger"} 
                         onClick={toggleMenu}

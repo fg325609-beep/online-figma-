924a6dfe530cd2bb963413d0c6a4c685a0ac66d0
Farhod Gofurov <fg325609@gmail.com>
2026-05-26 16:05:03 +0500

    llllll

--- src/companents/Sectiontwo/Sectiontwo.jsx
+++ src/companents/Sectiontwo/Sectiontwo.jsx
@@ -16,18 +16,22 @@ const Sectiontwo = () => {
                     <div className="card">
                       <img src="src/companents/img/Icon1.png" alt="" />
                       <h1>{t('how_it_work.card_1.title')}</h1>
+                      <p>{t('how_it_work.card_1.description')}</p>
                     </div>
                     <div className="card">
                       <img src="src/companents/img/icon3.png" alt="" />
                       <h1>{t('how_it_work.card_2.title')}</h1>
+                      <p>{t('how_it_work.card_2.description')}</p>
                     </div>
                     <div className="card">
                       <img src="src/companents/img/icon.png" alt="" />
                       <h1>{t('how_it_work.card_3.title')}</h1>
+                      <p>{t('how_it_work.card_3.description')}</p>
                     </div>
                     <div className="card">
                       <img src="src/companents/img/icon4.png" alt="" />
                       <h1>{t('how_it_work.card_4.title')}</h1>
+                      <p>{t('how_it_work.card_4.description')}</p>
                     </div>
                 </div>
         </div>

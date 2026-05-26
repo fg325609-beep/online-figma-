101cbce0448e156df851fac141480139e94dc894
Farhod Gofurov <fg325609@gmail.com>
2026-05-26 13:47:59 +0500

    lllll

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Header from './companents/Header/Header'
 import './i18n';
 import { Hero } from './companents/Hero/Hero';
 import Sectionone from './companents/Sectionone/Sectionone';
+import Sectiontwo from './companents/Sectiontwo/Sectiontwo';
+
 const App = () => {
   return (
     <div>
       <Header/>
       <Hero/>
       <Sectionone/>
+      <Sectiontwo/>
+
 
     </div>
   )
--- src/companents/Sectiontwo/Sectiontwo.jsx
+++ src/companents/Sectiontwo/Sectiontwo.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import "./Sectiontwo.scss"
+import { useTranslation } from 'react-i18next';
 const Sectiontwo = () => {
+    const { t, i18n } = useTranslation();
   return (
-    <div></div>
+    <>
+    <section>
+        <div className="sect-two">
+            <div className="sect-two-title">
+                <h1>{t('how_it_work.title')}</h1>
+                <p>{t('how_it_work.subtitle_1')}</p>
+                <p>{t('how_it_work.subtitle_2')}</p>
+                <div className="cards">
+                    <div className="crd"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
   )
 }
 

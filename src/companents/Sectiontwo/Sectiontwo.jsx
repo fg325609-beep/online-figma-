52ef2dcc03f6895a7357a12ba82f03cc2a9fe5c7
Farhod Gofurov <fg325609@gmail.com>
2026-05-26 14:35:12 +0500

    lllll

--- src/companents/Sectiontwo/Sectiontwo.jsx
+++ src/companents/Sectiontwo/Sectiontwo.jsx
@@ -8,13 +8,28 @@ const Sectiontwo = () => {
     <section>
         <div className="sect-two">
             <div className="sect-two-title">
-                <h1>{t('how_it_work.title')}</h1>
+                <h1 className='h1'>{t('how_it_work.title')}</h1>
                 <p>{t('how_it_work.subtitle_1')}</p>
                 <p>{t('how_it_work.subtitle_2')}</p>
-                <div className="cards">
-                    <div className="crd"></div>
-                </div>
             </div>
+                <div className="cards">
+                    <div className="card">
+                      <img src="src/companents/img/Icon1.png" alt="" />
+                      <h1>{t('how_it_work.card_1.title')}</h1>
+                    </div>
+                    <div className="card">
+                      <img src="src/companents/img/icon3.png" alt="" />
+                      <h1>{t('how_it_work.card_2.title')}</h1>
+                    </div>
+                    <div className="card">
+                      <img src="src/companents/img/icon.png" alt="" />
+                      <h1>{t('how_it_work.card_3.title')}</h1>
+                    </div>
+                    <div className="card">
+                      <img src="src/companents/img/icon4.png" alt="" />
+                      <h1>{t('how_it_work.card_4.title')}</h1>
+                    </div>
+                </div>
         </div>
     </section>
     </>

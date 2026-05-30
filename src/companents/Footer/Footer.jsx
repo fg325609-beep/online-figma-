2c17837c108d434db6d5541d642864cc287a67d0
Farhod Gofurov <fg325609@gmail.com>
2026-05-30 14:08:02 +0500

    llll

--- src/companents/Footer/Footer.jsx
+++ src/companents/Footer/Footer.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// Facebook ikonkasini brandlar to'plamidan import qilamiz
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'; 
+
 const Footer = () => {
-         const { t, i18n } = useTranslation();
-    
+  const { t } = useTranslation(); // 'i18n' ishlatilmagani uchun uni olib tashladik
+
   return (
     <>
-<footer>
-    <div className="fcontainer">
-       <div className="cards">
-        <div className="card">
-            <h1>{t('footer.education.title')}</h1>
-            <p>{t('footer.education.description')}</p>
-            <h2>{t('footer.follow_us')}</h2>
+      <footer>
+        <div className="fcontainer">
+          <div className="cards">
+            <div className="card">
+              <h1>{t('footer.education.title')}</h1>
+              <p>{t('footer.education.description')}</p>
+              <h2>{t('footer.follow_us')}</h2>
+              <span>
+                {/* To'g'ri import qilingan ikonkani icon propiga beramiz */}
+                <FontAwesomeIcon icon={faFacebookSquare} />
+              </span>
+            </div>
+          </div>
         </div>
-       </div>
-    </div>
-</footer>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
